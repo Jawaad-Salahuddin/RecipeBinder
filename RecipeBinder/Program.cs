39dddbcf4d9cf8978cb3f3bb32f3e6dda7d56c64
Jawaad Salahuddin <jsalahuddin@uwaterloo.ca>
2021-02-15 11:54:23 -0500

    added key vault

--- RecipeBinder/Program.cs
+++ RecipeBinder/Program.cs
@@ -1,5 +1,8 @@
+using Azure.Identity;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace RecipeBinder
 {
@@ -12,6 +15,13 @@ namespace RecipeBinder
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+                    config.AddAzureKeyVault(
+                    keyVaultEndpoint,
+                    new DefaultAzureCredential());
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
